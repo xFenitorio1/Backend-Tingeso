7503c24c8f00c9c5f50177e150730f7dccf17b67
Gerardo Pérez <gerardo.perez@usach.cl>
2026-04-02 08:46:45 -0300

    Inicialización proyecto

---
+++ src/main/java/org/example/tingesoback/TingesoBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.tingesoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TingesoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TingesoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tingesoback/TingesoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tingesoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TingesoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
